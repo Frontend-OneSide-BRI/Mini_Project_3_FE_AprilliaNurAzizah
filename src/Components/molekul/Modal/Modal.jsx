d6b244e3157c67b07e95843b35f3777dbe3fe518
aprillianurazizah <aprillia.azizah1@gmail.com>
2023-06-08 19:01:40 +0700

    add title, image, popularity, and overview in modal

--- src/Components/molekul/Modal/Modal.jsx
+++ src/Components/molekul/Modal/Modal.jsx
@@ -14,6 +14,19 @@ const ModalMovie = ({ isShow, onClose, data }) => {
           src={`https://image.tmdb.org/t/p/w500/${data.backdrop_path}`}
           alt={data.original_name}
         />
+        {data.release_date && (
+          <p className=" text-gray-900 mt-4">
+            Release Date: {data.release_date}
+          </p>
+        )}
+
+        {data.overview && (
+          <p className=" text-gray-900 mt-2">Overview: {data.overview}</p>
+        )}
+
+        {data.popularity && (
+          <p className=" text-gray-900 mt-2">Popularity: {data.popularity}</p>
+        )}
       </Modal.Body>
     </Modal>
   );
